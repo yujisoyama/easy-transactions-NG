e4d2db6e32f258dd4797b6e4781063a6194d342f
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-18 01:37:09 -0300

    Fixed Selection Tabs bug

--- web/src/components/DashboardTabs.tsx
+++ web/src/components/DashboardTabs.tsx
@@ -1,26 +1,20 @@
 import * as Tabs from '@radix-ui/react-tabs';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { TransactionsHistoryTab } from './TransactionsHistoryTab';
 import { TransferTab } from './TransferTab';
 
 
 export const DashboardTabs = () => {
-    const [tabOpen, setTabOpen] = useState<boolean[]>([true, false]);
-
-    const openTab = (tab: number) => {
-        const newTabOpen: boolean[] = [false, false];
-        newTabOpen[tab] = true;
-        setTabOpen(newTabOpen);
-    }
+    const [tabOpen, setTabOpen] = useState<boolean>(true);
 
     return (
         <div className='bg-background w-5/6 max-w-[800px] mb-10 rounded-lg mobile:mx-auto'>
-            <Tabs.Root className="flex flex-col py-4" defaultValue="transfer">
+            <Tabs.Root className="flex flex-col py-4" defaultValue="transfer" onValueChange={() => setTabOpen(previousTab => !previousTab)}>
                 <Tabs.List className="flex text-xl font-semibold">
-                    <Tabs.Trigger onClick={() => openTab(0)} className={`px-4 ml-4 border-b-2 ${tabOpen[0] ? 'text-highlightGreen border-highlightGreen' : 'text-paragraph border-background hover:cursor-pointer hover:text-main duration-150'} mobile:text-base`} value="transfer">
+                    <Tabs.Trigger className={`px-4 ml-4 border-b-2 ${tabOpen ? 'text-highlightGreen border-highlightGreen' : 'text-paragraph border-background hover:cursor-pointer hover:text-main duration-150'} mobile:text-base`} value="transfer">
                         Transfer
                     </Tabs.Trigger>
-                    <Tabs.Trigger onClick={() => openTab(1)} className={`px-4 border-b-2 ${tabOpen[1] ? 'text-highlightGreen border-highlightGreen' : 'text-paragraph border-background hover:cursor-pointer hover:text-main duration-150'} mobile:text-base`} value="history">
+                    <Tabs.Trigger className={`px-4 border-b-2 ${!tabOpen ? 'text-highlightGreen border-highlightGreen' : 'text-paragraph border-background hover:cursor-pointer hover:text-main duration-150'} mobile:text-base`} value="history">
                         Transactions History
                     </Tabs.Trigger>
                 </Tabs.List>
