1ae2e645c7d3656a32e5af7a20dfb389157f6c5e
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-18 16:55:21 -0300

    Final fixes

--- web/src/components/HomeComponents/HomeHeader.tsx
+++ web/src/components/HomeComponents/HomeHeader.tsx
@@ -2,7 +2,7 @@ import img from '../../../assets/NG-logo.png'
 
 export const HomeHeader = () => {
     return (
-        <div className="bg-background font-open flex flex-row items-center justify-between p-5">
+        <div className="bg-background font-open flex flex-row items-center justify-between px-5 h-[72px]">
             <img src={img} alt="NG" className='w-16 mobile:w-10' />
             <p className="text-lg text-paragraph mobile:text-base">Making transactions easy</p>
         </div>
